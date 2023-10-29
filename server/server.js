06c7b621bda5b4a8e09def42a374ec5b4f3a9847
HISHAN03 <moidinhishan@gmail.com>
2023-10-29 14:06:44 +0530

    Dotenv Update

--- server/server.js
+++ server/server.js
@@ -8,13 +8,20 @@ const io = require('socket.io')(server, {
     methods: ['GET', 'POST'],
   },
 });
+require('dotenv').config();
 
-mongoose.connect("mongodb+srv://hishan:1234@cluster0.sksy2nt.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-
-
 })
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+
 const defaultValue = ""
 let dname
 io.on('connection', (socket) => {
